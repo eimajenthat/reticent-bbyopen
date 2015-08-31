3bca6339a5edb717bef6129334cc3c5a92461e76
Jamie Adams <jamie@ctschq.com>
2015-08-30 21:42:47 -0500

    Pull user's location and user it to update store list.

--- resources/assets/js/controllers/StoreListCtrl.js
+++ resources/assets/js/controllers/StoreListCtrl.js
@@ -4,17 +4,43 @@ angular.module('reticentDemoApp.controllers')
         $scope.radius = 50;
         $scope.map = { center: { latitude: 0, longitude: 0 }, zoom: 10 };
 
-        $scope.$watchCollection('[location,radius]',function(newVal){
-            var geocoder = new google.maps.Geocoder();
-            geocoder.geocode({'address': newVal[0]}, function (result) {
-                console.log(result[0]);
-                $scope.map.center.latitude = result[0].geometry.location.lat();
-                $scope.map.center.longitude = result[0].geometry.location.lng();
-                $scope.stores = Store.query({location:result[0].geometry.location.lat()+','+result[0].geometry.location.lng(), radius:newVal[1]});
+        var geocoder = new google.maps.Geocoder();
 
-                $scope.$apply();
+        $scope.$watchCollection('[location,radius]',function(newVal){
+            geocoder.geocode({'address': newVal[0]}, function (result, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                    $scope.map.center.latitude = result[0].geometry.location.lat();
+                    $scope.map.center.longitude = result[0].geometry.location.lng();
+                    $scope.stores = Store.query({
+                        location: result[0].geometry.location.lat() + ',' + result[0].geometry.location.lng(),
+                        radius: newVal[1]
+                    });
+
+                    $scope.$apply();
+                }
             });
-        })
+        });
+
+
+        navigator.geolocation.getCurrentPosition(function(position){
+            console.log(position);
+            var latlng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+            geocoder.geocode({'latLng': latlng}, function(results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                    console.log(results);
+                    var result = results[0];
+                    for(var i=0, len=result.address_components.length; i<len; i++) {
+                        var ac = result.address_components[i];
+                        if(ac.types.indexOf("postal_code") >= 0) {
+                            $scope.location = ac.long_name;
+                            console.log(ac.long_name);
+                            $scope.$apply();
+                            break;
+                        }
+                    }
+                }
+            });
+        });
 
 
     }]
